a9f9706f5960fa643e7a29f4caa73ec0771163a5
unknown <BalajiDasari@TTL001488.thetrainline.com>
2015-01-27 19:55:27 +0000

    added new sample file

---
+++ sample/calculator/CalculatorTest/OperationTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class OperationTest
+    {
+        [TestMethod]
+        public void WantToTestAddFunctionality()
+        {
+            var operation = new Operation();
+            int sum = operation.Add(2, 2);
+            Assert.AreEqual(4,sum);
+        }
+
+        [TestMethod]
+        public void WantToTestSubtractFunctionality()
+        {
+            var operation = new Operation();
+            int sub = operation.Sub(4, 2);
+            Assert.AreEqual(2,sub);
+        }
+
+        [TestMethod]
+        public void WantToTestDivisonFunctionality()
+        {
+            var operation = new Operation();
+            var div = operation.Div(4, 2);
+            Assert.AreEqual(2, div);
+        }
+
+        [TestMethod]
+        public void WantToTestDecimal()
+        {
+            var operation = new Operation();
+            var div = operation.Div(3, 2);
+            Assert.AreEqual(1.5, div);
+        }
+
+        [TestMethod]
+        public void WantToTestMul()
+        {
+            var operation = new Operation();
+            var mul = operation.Mul(3, 2);
+            Assert.AreEqual(6, mul);
+        }
+    }
+}
---
+++ sample/calculator/calculator/Operation.cs
@@ -0,0 +1,26 @@
+﻿namespace calculator
+{
+    public class Operation
+    {
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int input1, int input2)
+        {
+            return input1 - input2;
+        }
+
+        public double Div(double i, double i1)
+        {
+            return i/i1;
+        }
+
+        public double Mul(int i, int i1)
+        {
+            return i * i1;
+        }
+    }
+}
